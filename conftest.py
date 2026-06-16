d0f57ce6fc8e12088adc40d6a483d5132e3182f4
Deepan20 <deepz2k4@gmail.com>
2026-06-17 01:35:51 +0530

    actions 2

--- conftest.py
+++ conftest.py
@@ -4,13 +4,13 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="class")
 def driver():
-    options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
+    # options = Options()
+    # options.add_argument("--headless=new")
+    # options.add_argument("--no-sandbox")
+    # options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("--disable-gpu")
 
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
     driver.get("https://www.leafground.com/")
 
     yield driver
